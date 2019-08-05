22861a7c807efbb71c01ef6d9fd6c151111c3bee
benirexima <52735980+benirexima@users.noreply.github.com>
2019-08-05 17:59:40 +0530

    Add files via upload

---
+++ Jayashe.java
@@ -0,0 +1,16 @@
+package reverseString;
+
+public class Jayashe {
+	public static void main(String args[]) {
+		
+	
+	
+	for(int i=0;i<5;i++) {
+		int a=i+1;
+		int b=i+2;
+		int c=i+3;
+		System.out.println(a+""+b+""+c);
+		
+	}
+	}
+}
